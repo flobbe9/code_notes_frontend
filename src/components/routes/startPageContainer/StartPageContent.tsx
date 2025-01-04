b8a2e4b1224b529eaa33d5cbcbbe454d61f0d503
flobbe9 <florin735@live.com>
2025-01-04 22:56:00 +0100

    fix: handle search only if logged in

--- src/components/routes/startPageContainer/StartPageContent.tsx
+++ src/components/routes/startPageContainer/StartPageContent.tsx
@@ -176,7 +176,7 @@ export default function StartPageContent({...props}: Props) {
     function handleSearch(searchValue = noteSearchValue): void {
 
         // if edited notes
-        if (editedNoteIds.size) {
+        if (editedNoteIds.size && isLoggedIn) {
             toast("Cannot search", "Please save your pending changes first.", "warn");
             return;
         }
